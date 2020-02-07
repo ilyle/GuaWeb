3a1433a4bcba9093cbba8386bc9f7c1ba2229353
Xujie <jie.xu@coer-scnu.org>
2020-02-07 12:08:49 +0800

    初始化工程

---
+++ src/main/java/com/xiaoqi/guaweb/GuawebApplication.java
@@ -0,0 +1,13 @@
+package com.xiaoqi.guaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuawebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiaoqi/guaweb/GuawebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaoqi.guaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
